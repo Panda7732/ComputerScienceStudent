61ceac59a9cc95b0627017d259d1d983eea6f193
Panda7732 <77357480+Panda7732@users.noreply.github.com>
2021-03-19 19:58:22 -0400

    Add files via upload

---
+++ Computer Science Student/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Computer_Science_Student
+{
+    abstract class Student
+    {
+        //fields
+        private string _name;
+        private string _id;
+
+        //constructor
+        public Student(string name,string id)
+        {
+            _name = name;
+            _id = id;
+        }
+
+        //Name property
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        //RequiredHours property (abstract)
+        public abstract double RequiredHours
+        {
+            get;
+        }
+
+
+    }
+}
